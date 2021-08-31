dbf8ce5edcff69d768774ba5490746a6293e50b6
frank <frankli84@outlook.com>
2021-08-30 19:20:22 -0700

    add gqlAuthGuard to change password and update user for authorization

--- src/components/users/users.resolver.ts
+++ src/components/users/users.resolver.ts
@@ -1,9 +1,14 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
-import { Console } from 'console';
+import { Args, Mutation, Query, Resolver, Context } from '@nestjs/graphql';
 import { UsersService } from './users.services';
 import { NewUserInput } from './dto/new-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
+import { changeUserPWInput } from './dto/update-pw.input';
 import { User } from './entities/user';
+import { CurrentUser } from './users.decorator';
+import { userLoginInput } from './dto/user-login.input';
+import { SimpleConsoleLogger } from 'typeorm';
+import { HttpException, HttpStatus, NotFoundException, UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from './auth.guard';
 
 
 @Resolver()
@@ -26,48 +31,81 @@ export class UsersResolver {
 
   @Query((returns) => User)
   public async finduserByEmail(@Args('email') email: string): Promise<User> {
-    return await this.userService.findByUsername(email).catch((err) => {
+    return await this.userService.findByUserEmail(email).catch((err) => {
       throw err;
     });
   }
-
-  @Mutation(() => Boolean)
-  public async deleteUser(@Args('email') email: string) {
-    return await this.userService.deleteUser(email).catch((err) => {
-      throw err;
-    });
+  // use mutation create jwt for user login request
+  @Mutation((returns) => String)
+  public  async login(
+    @Args('email') email: string,
+    @Args('password') password: string,
+  ) {
+    try {
+      return await this.userService.login(email,password);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  
+  @Query(() => User)
+  @UseGuards(GqlAuthGuard)
+  public async CurrentUser(@CurrentUser() user: User) {
+    try {
+      return await this.userService.getUserById(user.id);
+    } catch (err) {
+      console.error(err);
+    }
   }
 
   @Mutation((returns) => User)
   public async addNewUser(
     @Args('newUserData') newUserData: NewUserInput,
   ): Promise<User> {
-    return await this.userService.addUser(newUserData).catch((err) => {
+    return await this.userService.createUser(newUserData).catch((err) => {
       throw err;
     });
   }
 
+  @Mutation(() => User)
+  @UseGuards(GqlAuthGuard)
+  async UpdateUserPass(
+    @CurrentUser() user: User,
+    @Args('currPass') currPass: string,
+    @Args('newPass') newPass: string,
+  ) {
+    try {
+      return await this.userService.updatePassword(user.id,currPass,newPass);
+    } catch (err) {
+      console.error(err);
+    }
+  }
   @Mutation((returns) => User)
-  public async updateUserInfo(@Args('email') email: string,
+  @UseGuards(GqlAuthGuard)
+  public async updateUserInfo(@CurrentUser() user: User,
     @Args('updateData') updateData: UpdateUserInput,
   ): Promise<User> {
-    return await this.userService.updateUserInfo(email, updateData).catch((err) => {
+    return await this.userService.updateUserInfo(user.id, updateData).catch((err) => {
       throw err;
     });
   }
 
+  @Mutation(() => Boolean)
+  @UseGuards(GqlAuthGuard)  
+  public async deleteUser(@CurrentUser() user: User) {
+    return await this.userService.deleteUser(user.id).catch((err) => {
+      throw err;
+    });
+  }
+  
+  //will delete this function at production code
   @Mutation((returns) => Boolean)
-  public async deleteByEmail(@Args('email') email: string,) {
-    return await this.userService.deleteUser(email).catch((err) => {
+  public async deleteAllUsers() {
+    return await this.userService.deleteAllUsers().catch((err) => {
       throw err;
     });
   }
-
-    @Mutation((returns) => Boolean)
-    public async deleteAllUsers() {
-      return await this.userService.deleteAllUsers().catch((err) => {
-        throw err;
-      });
-    }
 
 }
+
+
